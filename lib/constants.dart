2ad8b6f07815b51b85fc3f85790b81439cce04f0
Loong Yeat <pongloongyeat@gmail.com>
2022-05-21 14:36:04 +0800

    Documentation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,11 @@
+/// Generic API status codes.
 class ApiStatusCodes {
+  /// The success code for a successful API call.
   static const success = 200;
 }
 
+/// PokéAPI-related defaults.
 class ApiDefaults {
+  /// The base URL for all PokéAPI API calls.
   static const baseUrl = 'https://pokeapi.co/api/v2';
 }

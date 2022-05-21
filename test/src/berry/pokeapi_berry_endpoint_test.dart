ad305abe2f5a2d4665b0453321b710f329313aac
Loong Yeat <pongloongyeat@gmail.com>
2022-05-21 19:37:38 +0800

    Add tests for other ways of getting data

--- test/src/berry/pokeapi_berry_endpoint_test.dart
+++ test/src/berry/pokeapi_berry_endpoint_test.dart
@@ -3,20 +3,33 @@ import 'package:test/test.dart';
 
 void main() async {
   final client = PokeApi();
-  final result = await client.berry.getById(1);
-  final resultJson = result?.toJson();
+  final resultById = await client.berry.getById(1);
+  final resultByIdJson = resultById?.toJson();
+
+  final resultByName = await client.berry.getByQuery(resultById?.name ?? '');
+  final resultByNameJson = resultByName?.toJson();
 
   group('PokeApiBerryEndpoint', () {
     test('successfully fetches data', () {
-      expect(result, isNotNull);
+      expect(resultById, isNotNull);
+      expect(resultByName, isNotNull);
     });
 
     test('toJson returns non-null Map', () {
-      expect(resultJson, isNotNull);
+      expect(resultByIdJson, isNotNull);
+      expect(resultByNameJson, isNotNull);
     });
 
     test('fromJson instantiates class correctly', () {
-      expect(PokeApiBerry.fromJson(resultJson!), result);
+      expect(PokeApiBerry.fromJson(resultByIdJson!), resultById);
+      expect(PokeApiBerry.fromJson(resultByNameJson!), resultById);
+    });
+
+    test('fromJson instantiates the same class by ID and by name', () {
+      expect(
+        PokeApiBerry.fromJson(resultByIdJson!),
+        PokeApiBerry.fromJson(resultByNameJson!),
+      );
     });
   });
 }

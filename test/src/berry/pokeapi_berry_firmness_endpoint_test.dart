64e3a788f45e1c92cb1e37de23cecccac5ef8e4c
Loong Yeat <pongloongyeat@gmail.com>
2022-05-21 20:17:50 +0800

    Add tests for other berry endpoints

---
+++ test/src/berry/pokeapi_berry_firmness_endpoint_test.dart
@@ -0,0 +1,50 @@
+import 'package:pokeapi_dart_wrapper/pokeapi_dart_wrapper.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  final client = PokeApi();
+  final resultById = await client.berryFirmness.getById(1);
+  final resultByIdJson = resultById?.toJson();
+
+  final resultByQuery = await client.berryFirmness.getByQuery(
+    resultById?.name ?? '',
+  );
+  final resultByQueryJson = resultByQuery?.toJson();
+
+  final resultByPagination = await client.berryFirmness.getPaginated();
+  final resultByPaginationJson = resultByPagination?.toJson();
+
+  group('PokeApiBerryFirmnessEndpoint', () {
+    test('successfully fetches data', () {
+      expect(resultById, isNotNull);
+      expect(resultByQuery, isNotNull);
+      expect(resultByPagination, isNotNull);
+    });
+
+    test('toJson returns non-null Map', () {
+      expect(resultByIdJson, isNotNull);
+      expect(resultByQueryJson, isNotNull);
+      expect(resultByPaginationJson, isNotNull);
+    });
+
+    test('fromJson instantiates class correctly', () {
+      expect(PokeApiBerryFirmness.fromJson(resultByIdJson!), resultById);
+      expect(PokeApiBerryFirmness.fromJson(resultByQueryJson!), resultByQuery);
+      expect(
+        PokeApiNamedApiResourceList.fromJson(resultByPaginationJson!),
+        resultByPagination,
+      );
+    });
+
+    test('fetched class are same for by ID and by query', () {
+      expect(resultById, resultByQuery);
+    });
+
+    test('fromJson instantiates the same class by ID and by query', () {
+      expect(
+        PokeApiBerryFirmness.fromJson(resultByIdJson!),
+        PokeApiBerryFirmness.fromJson(resultByQueryJson!),
+      );
+    });
+  });
+}
---
+++ test/src/berry/pokeapi_berry_flavor_endpoint_test.dart
@@ -0,0 +1,50 @@
+import 'package:pokeapi_dart_wrapper/pokeapi_dart_wrapper.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  final client = PokeApi();
+  final resultById = await client.berryFlavor.getById(1);
+  final resultByIdJson = resultById?.toJson();
+
+  final resultByQuery = await client.berryFlavor.getByQuery(
+    resultById?.name ?? '',
+  );
+  final resultByQueryJson = resultByQuery?.toJson();
+
+  final resultByPagination = await client.berryFlavor.getPaginated();
+  final resultByPaginationJson = resultByPagination?.toJson();
+
+  group('PokeApiBerryFlavorEndpoint', () {
+    test('successfully fetches data', () {
+      expect(resultById, isNotNull);
+      expect(resultByQuery, isNotNull);
+      expect(resultByPagination, isNotNull);
+    });
+
+    test('toJson returns non-null Map', () {
+      expect(resultByIdJson, isNotNull);
+      expect(resultByQueryJson, isNotNull);
+      expect(resultByPaginationJson, isNotNull);
+    });
+
+    test('fromJson instantiates class correctly', () {
+      expect(PokeApiBerryFlavor.fromJson(resultByIdJson!), resultById);
+      expect(PokeApiBerryFlavor.fromJson(resultByQueryJson!), resultByQuery);
+      expect(
+        PokeApiNamedApiResourceList.fromJson(resultByPaginationJson!),
+        resultByPagination,
+      );
+    });
+
+    test('fetched class are same for by ID and by query', () {
+      expect(resultById, resultByQuery);
+    });
+
+    test('fromJson instantiates the same class by ID and by query', () {
+      expect(
+        PokeApiBerryFlavor.fromJson(resultByIdJson!),
+        PokeApiBerryFlavor.fromJson(resultByQueryJson!),
+      );
+    });
+  });
+}
